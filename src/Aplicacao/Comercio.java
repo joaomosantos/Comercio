29c9a704318810924d5de43e0ed47518dd1fd79d
Max Oliveira <Max Oliveira@DESKTOP-0K37T0T>
2015-08-23 13:22:12 -0300

    - [ADD] Conexão com o banco de dados SQL Server

--- src/Aplicacao/Comercio.java
+++ src/Aplicacao/Comercio.java
@@ -1,9 +1,11 @@
 package Aplicacao;
+import Dao.dbaConexao;
 
 public class Comercio {
 
     public static void main(String[] args) {
-        
+        dbaConexao conexao = new dbaConexao();
+        conexao.conectar();
     }
-    
+
 }
