2eff2e861b16ade68cb75852611d978be06b2bca
max.oliveira <max.oliveira@ONE-DT52.one.intranet>
2015-08-24 16:06:47 -0300

    - [ALTER] Estavel 100% - Login Acessando direto ao banco

---
+++ src/Dao/DBAConexao.java
@@ -0,0 +1,37 @@
+package Dao;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class DBAConexao {
+    public Statement stm; // Responsavel por preparar e realizar pesquisas no banco de dados;
+    public ResultSet rs; // Responsavel por armazenar o resultado de um pesquisa passada para o statement;
+    private String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private String caminho = "jdbc:sqlserver://localhost:1433;databaseName=DB_COMERCIO"; // O "DB_COMERCIO" representa a minha database 
+    private String usuario = "admin";
+    private String senha = "admin";
+    public Connection conexao; // Responsavel por realizar a conexão com o banco de dados;
+    
+    public void conectar() { // Metodo responsavel por realizar a conexão;
+        try {
+            System.setProperty("jdbc.Drivers", driver); // Seta a propriedade do driver de conexão;
+            conexao = DriverManager.getConnection(caminho, usuario, senha); // Realiza a conexão com o banco;
+            //JOptionPane.showMessageDialog(null, "Conectado com sucesso!", "Banco de Dados", JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBAConexao.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Erro de conexão!\nERRO: " + ex.getMessage(), "Banco de Dados", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+    
+    public void desconectar() { // Metodo responsavel por fechar a conexão
+        try {
+            conexao.close(); // Fechar conexão
+            //JOptionPane.showMessageDialog(null, "Conexão fechada com sucesso!", "Banco de Dados", JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBAConexao.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Erro ao fechar a conexão!\nERRO: " + ex.getMessage(), "Banco de Dados", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+}
